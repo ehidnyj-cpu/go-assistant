35ce7a93ebc5304c74a5dd8fd80ebed5269a4ba2
Dmitry <ehidnyj@gmail.com>
2026-02-11 22:06:22 +0300

    День 2. Изучал ввод данных пользователем и циклы с условиями

--- assistent/logic.go
+++ assistent/logic.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func Greet(name string) string {
 	u := User{Name: name} //struct is model.go
-	return fmt.Sprintf("Привет, %s! Проект живой ✅", u.Name)
+	return fmt.Sprintf("Привет, %s! Рад познакомиться", u.Name)
 }
--- assistent/main.go
+++ assistent/main.go
@@ -1,11 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio" //пакет для чтения ввода
+	"fmt"
+	"os"           // доступ к стандартному вводу (stdin)
+	"strings"      // работа со строками
+	"unicode/utf8" //для подсчета символов на кириллице
+)
 
 func main() {
-	fmt.Println("assistent:start")
 
-	// вызов функции из logic.go
-	msg := Greet("Дима")
-	fmt.Println(msg)
+	fmt.Println()
+	fmt.Println("assistent:start")
+	fmt.Println()
+
+	reader := bufio.NewReader(os.Stdin) // Создаем reader для чтения файла
+
+	var name string
+
+	for {
+
+		fmt.Println("Как тебя зовут?") // Print без перевода строки
+		fmt.Println()
+		name, _ = reader.ReadString('\n') // Читаем строку до нажатия Enter
+
+		name = strings.TrimSpace(name) //убираем \n и лишние пробелы
+
+		fmt.Println()
+
+		if name == "" {
+			fmt.Println("Имя не введено, попробуй ещё раз.")
+			fmt.Println()
+			continue
+		}
+
+		length := utf8.RuneCountInString(name)
+
+		if length < 2 {
+			fmt.Println("Слишком коротко для имени :)")
+			fmt.Println()
+			continue
+		}
+
+		if length > 15 {
+			fmt.Println("Слишком длинное имя я так не запомню :)")
+			fmt.Println()
+			continue
+		}
+
+		msg := Greet(name) // Вызываем нашу функцию логики
+		fmt.Println(msg)
+		fmt.Println()
+		break
+	}
+
+	fmt.Println("len:", len(name))
+	fmt.Println("runes", utf8.RuneCountInString(name))
+
 }
