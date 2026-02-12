018f3bb8d0fa17d14003a0209e4d409b9a66b72e
Dmitry <ehidnyj@gmail.com>
2026-02-12 22:50:46 +0300

    Day3: add command handling with switch

--- assistent/logic.go
+++ assistent/logic.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
-func Greet(name string) string {
-	u := User{Name: name} //struct is model.go
-	return fmt.Sprintf("Привет, %s! Рад познакомиться", u.Name)
+func Respond(input string) string {
+	input = strings.ToLower(strings.TrimSpace(input))
+
+	switch input {
+
+	case "привет":
+		return "Привет! Рад тебя видеть!"
+
+	case "пока":
+		return "До встречи"
+
+	case "время":
+		return fmt.Sprintf("Сейчас %s", time.Now().Format("15:04:05"))
+
+	case "hola":
+		return "Buenos"
+
+	case "кто ты":
+		return "Я твой ассистент"
+
+	default:
+		return "Я пока не понимаю эту команду"
+
+	}
+
 }
--- assistent/main.go
+++ assistent/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio" //пакет для чтения ввода
 	"fmt"
-	"os"           // доступ к стандартному вводу (stdin)
-	"strings"      // работа со строками
-	"unicode/utf8" //для подсчета символов на кириллице
+	"os"      // доступ к стандартному вводу (stdin)
+	"strings" // работа со строками
+	//для подсчета символов на кириллице
 )
 
 func main() {
@@ -16,45 +16,27 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin) // Создаем reader для чтения файла
 
-	var name string
-
 	for {
 
-		fmt.Println("Как тебя зовут?") // Print без перевода строки
+		fmt.Print("Введи команду: ")
+
+		input, err := reader.ReadString('\n')
 		fmt.Println()
-		name, _ = reader.ReadString('\n') // Читаем строку до нажатия Enter
-
-		name = strings.TrimSpace(name) //убираем \n и лишние пробелы
-
-		fmt.Println()
-
-		if name == "" {
-			fmt.Println("Имя не введено, попробуй ещё раз.")
-			fmt.Println()
+		if err != nil {
+			fmt.Println("Ошибка:", err)
 			continue
 		}
 
-		length := utf8.RuneCountInString(name)
+		input = strings.TrimSpace(input)
 
-		if length < 2 {
-			fmt.Println("Слишком коротко для имени :)")
-			fmt.Println()
-			continue
+		if input == "выход" {
+			fmt.Println("Завершение работы")
+			break
 		}
 
-		if length > 15 {
-			fmt.Println("Слишком длинное имя я так не запомню :)")
-			fmt.Println()
-			continue
-		}
-
-		msg := Greet(name) // Вызываем нашу функцию логики
-		fmt.Println(msg)
+		response := Respond(input)
+		fmt.Println(response)
 		fmt.Println()
-		break
 	}
-
-	fmt.Println("len:", len(name))
-	fmt.Println("runes", utf8.RuneCountInString(name))
 
 }
